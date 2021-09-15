cb534f8e0c226de27af69fbd33e8792de8115b7a
Saurabh <saurabhdesale007@gmail.com>
2021-09-15 12:47:27 +0530

    added: edit existing contact

--- src/com/blz/AddressBook.java
+++ src/com/blz/AddressBook.java
@@ -14,7 +14,7 @@ public class AddressBook {
 		ArrayList<Contact> contactList = new ArrayList<>();
 		int choice = 0;
 		do {
-			System.out.println("1.add\n2.display\n3.exit");
+			System.out.println("1.add\n2.display\n3.exit\n 4.update");
 			System.out.println("Enter choice");
 			choice = SC.nextInt();
 			switch (choice) {
@@ -31,6 +31,34 @@ public class AddressBook {
 
 			case 3:
 				System.out.println("Thank You..");
+				break;
+
+			case 4:
+				System.out.println("Enter name which you want to replace: ");
+				String nameReplace = SC.next();
+				for (int i = 0; i < contactList.size(); i++) {
+					System.out.println(contactList.get(i));
+					Contact editContact = contactList.get(i);
+					if (nameReplace.equals(editContact.getFirstName())) {
+						System.out.println("Enter a first name: ");
+						editContact.setFirstName(SC.next());
+						System.out.println("Enter a last name: ");
+						editContact.setLastName(SC.next());
+						System.out.println("Enter an address: ");
+						editContact.setAddress(SC.next());
+						System.out.println("Enter a city: ");
+						editContact.setCity(SC.next());
+						System.out.println("Enter a state: ");
+						editContact.setState(SC.next());
+						System.out.println("Enter a zip: ");
+						editContact.setZip(SC.nextInt());
+						System.out.println("Enter a phone number: ");
+						editContact.setPhoneNumber(SC.nextLong());
+						System.out.println("Enter an email ID: ");
+						editContact.setEmailID(SC.next());
+						contactList.set(i, editContact);
+					}
+				}
 				break;
 			default:
 				System.out.println("Please Enter from choice ");
